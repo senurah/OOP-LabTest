03952aa0c89167fb4c61f882e0e91779ece287b1
senurah <neophytetoskill@gmail.com>
2024-12-14 23:40:14 +0530

    feat: implemented the Invalid Configuration Exception.

---
+++ src/main/java/com/senurah/lab_test/exceptions/InvalidConfigurationException.java
@@ -0,0 +1,7 @@
+package com.senurah.lab_test.exceptions;
+
+public class InvalidConfigurationException extends Exception {
+    public InvalidConfigurationException(String message) {
+        super(message);
+    }
+}
