5fa099e0da7dde9ae7b10bb6fc5e0d908cc3f088
senurah <neophytetoskill@gmail.com>
2024-12-14 19:02:09 +0530

    feat: Customized the Vendor run method.

--- src/main/java/com/senurah/lab_test/threads/Vendor.java
+++ src/main/java/com/senurah/lab_test/threads/Vendor.java
@@ -12,19 +12,55 @@ public class Vendor extends AbstractTicketHandler implements Runnable {
         super(ticketPool);
         this.ticketReleaseRate = ticketReleaseRate;
     }
+
+    // Given in the pdf
+//    @Override
+//    public void run() {
+//        for (int i = 0; i < ticketReleaseRate; i++) {
+//            String ticket = "Ticket-" + System.nanoTime();
+//            ticketPool.addTickets(ticket);
+//            Logger.log("Vendor added: " + ticket);
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                Logger.log("Vendor interrupted.");
+//            }
+//        }
+//    }
+
+
     @Override
     public void run() {
-        for (int i = 0; i < ticketReleaseRate; i++) {
-            String ticket = "Ticket-" + System.nanoTime();
-            ticketPool.addTickets(ticket);
-            Logger.log("Vendor added: " + ticket);
+        while (true) {
+            synchronized (ticketPool) {
+                if (ticketPool.getTicketCount() >= ticketPool.getMaxTicketCapacity()) {
+                    Logger.log("Ticket pool is full. Vendor waiting...");
+                    try {
+                        ticketPool.wait();
+                    } catch (InterruptedException e) {
+                        Logger.log("Vendor interrupted.");
+                        break;
+                    }
+                } else if (ticketPool.areTicketsAvailable()) {
+                    String ticket = "Ticket-" + System.nanoTime();
+                    ticketPool.addTickets(ticket);
+                    Logger.log("Vendor added: " + ticket+"Tickets remaining :"+ticketPool.getTotalTickets());
+                    ticketPool.notifyAll(); // Notify customers
+                } else {
+                    Logger.log("No more tickets to release. Vendor stopping...");
+                    break;
+                }
+            }
+
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 Logger.log("Vendor interrupted.");
+                break;
             }
         }
     }
+
     @Override
     public void handleTickets() {
         run();
