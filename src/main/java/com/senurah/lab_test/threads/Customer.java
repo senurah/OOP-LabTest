c08b0241657376a879f82eaa8baee8bc82b84b74
senurah <neophytetoskill@gmail.com>
2024-12-14 19:01:59 +0530

    feat: Customized the Customer run method.

--- src/main/java/com/senurah/lab_test/threads/Customer.java
+++ src/main/java/com/senurah/lab_test/threads/Customer.java
@@ -9,23 +9,40 @@ public class Customer extends AbstractTicketHandler implements Runnable {
     public Customer(TicketPool ticketPool) {
         super(ticketPool);
     }
+
     @Override
     public void run() {
         while (true) {
-            String ticket = ticketPool.removeTicket();
-            if (ticket != null) {
-                Logger.log("Customer retrieved: " + ticket);
-            } else {
-                Logger.log("Customer found no tickets available.");
-                break;
+            synchronized (ticketPool) {
+                if (!ticketPool.areTicketsAvailable()) {
+                    Logger.log("No tickets available. Customer waiting...");
+                    try {
+                        ticketPool.wait();
+                    } catch (InterruptedException e) {
+                        Logger.log("Customer interrupted.");
+                        break;
+                    }
+                } else {
+                    String ticket = ticketPool.removeTicket();
+                    if (ticket != null) {
+                        Logger.log("Customer retrieved: " + ticket);
+                        ticketPool.notifyAll(); // Notify vendors
+                    } else {
+                        Logger.log("No more tickets left. Customer stopping...");
+                        break;
+                    }
+                }
             }
+
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 Logger.log("Customer interrupted.");
+                break;
             }
         }
     }
+
     @Override
     public void handleTickets() {
         run();
